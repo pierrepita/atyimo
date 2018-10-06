33632cea1674a861c55b4c98e34e6fb4c669f05f
pierrepita <pierre.pita@gmail.com>
2018-10-06 00:29:17 -0300

    config.py edited to expected new atyimo path.

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 ##----##----##----##----##----##----##----##----##----##----##----##----##----##--------##----##----##----##----##----##----
 status_larger_base = 1 #Boolean indicating execution of larger base
 status_smaller_base = 1 #Boolean indicating execution of small base
-default_folder = "results/" #main folder of each execution
-atyimo_path = "/home/pierre/Mega/atyimo_git/gitlab_pierrepita/atyimo/"
+default_folder = "results/" #main folder of each execution #remember to create this folder
+atyimo_path = "/path/to/atyimo/"
 
 ##----##----##----##----##----##----##----##----##----##----##----##----##----##--------##----##----##----##----##----##----
 # Set everything to be used in preprocessing module
